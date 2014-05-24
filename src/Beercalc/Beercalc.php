b3be04c431a4b154225c9646ed05d8b9a070dd6b
griffithben <griffith.ben@gmail.com>
2014-05-24 00:09:45 -0500

    init commit

---
+++ src/Beercalc/Beercalc.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Beercalc;
+
+class Beercalc{
+
+  /* ABV()
+  #  param og: number - original gravity
+  #  param fg: number - final gravity
+  */
+  public static function abv($og, $fg){
+    if($og > $fg && is_numeric($og) && is_numeric($fg))
+      return ($og - $fg) * 131;
+    else
+      return null;
+  }
+
+  /* ABW()
+  #  param og: number - original gravity
+  #  param fg: number - final gravity
+  #  SOURCE = http://hbd.org/ensmingr/
+  */
+  public static function abw($og, $fg){
+    if($og > $fg && is_numeric($og) && is_numeric($fg))
+      return (0.79 * self::abv($og, $fg)) / $fg;
+    else
+      return null;
+  }
+
+  /* MCU()
+  #  param weight: number - lbs of grain
+  #  param lovibond: number - typically a number between
+  #  param volume: number - gallons
+  */
+  public static function mcu($weight, $lovibond, $volume){
+    if($volume != 0 && is_numeric($weight) && is_numeric($lovibond) && is_numeric($volume))
+      return ($weight * $lovibond) / $volume;
+    else
+      return null;
+  }
+
+  /* SRM()
+  #  param weight: number - lbs of grain
+  #  param lovibond: number - typically a number between 1-25
+  #  param volume: number - gallons
+  */
+  public static function srm($weight, $lovibond, $volume){
+    if(is_numeric($weight) && is_numeric($lovibond) && is_numeric($volume))
+      return 1.4922 * (pow(self::mcu($weight, $lovibond, $volume), 0.6859));
+    else
+      return null;
+  }
+
+  /* AAU()
+  #  param alpha: percent - alpha unit of the hop
+  #  param weight: number - oz of hops
+  */
+  public static function aau($alpha, $weight){
+    if(is_numeric($alpha) && is_numeric($weight))
+      return $alpha * $weight;
+    else
+      return null;
+  }
+
+  /* IBU()
+  #  param alpha: percent - alpha unit of the hop
+  #  param weight: number - oz of hops
+  #  param utilization: number - output of self.utilization
+  #  param volume: number - gallons
+  */
+  public static function ibu($alpha, $weight, $time, $gravity, $volume){
+    if($volume != 0 && is_numeric($alpha) && is_numeric($weight) && is_numeric($time) && is_numeric($gravity) && is_numeric($volume))
+      return self::aau($alpha, $weight) * self::utilization($time, $gravity) * 75 / $volume;
+    else
+      return null;
+  }
+
+  /* UTILIZATION()
+  #  param time: number - minute hops are in the boil
+  #  param gravity: number - gravity of the boil upon inserting Ex. 1.050
+  */
+  public static function utilization($time, $gravity){
+    if(is_numeric($time) && is_numeric($gravity))
+      return (1.65 * pow(0.000125,($gravity - 1))) * (1 - pow(M_E,(-0.04 * $time))) / 4.15;
+    else
+      return null;
+  }
+
+  /* PLATO
+  #  param sGravity: number - specific gravity
+  #  SOURCE = http://hbd.org/ensmingr/
+  */
+  public static function plato($sGravity){
+    if(is_numeric($sGravity))
+      return (-463.37) + (668.72 * $sGravity) - (205.35 * pow($sGravity,2));
+    else
+      return null;
+  }
+
+  /* REAL EXTRACT
+  #  param og: number - original gravity
+  #  param fg: number - final gracivity
+  #  SOURCE = http://hbd.org/ensmingr/
+  */
+  public static function realExtract($og, $fg){
+    if($og > $fg && is_numeric($og) && is_numeric($fg))
+      return(0.1808 * self::plato($og)) + (0.8192 * self::plato($fg));
+    else
+      return null;
+  }
+
+  /* CALORIES (in 12 ounce increments)
+  #  param og: number - original gravity
+  #  param fg: number - final gravity
+  #  SOURCE = http://hbd.org/ensmingr/
+  */
+  public static function calories($og, $fg){
+    if($og > $fg && is_numeric($og) && is_numeric($fg))
+      return ((6.9 * self::abw($og, $fg)) + 4.0 * (self::realExtract($og, $fg) - 0.1)) * $fg * 3.55;
+    else
+      return null;
+  }
+
+  /* ATTENUATION
+  #  param og: number - original gravity
+  #  param fg: number - final gracivity
+  #  Assuming this is in gravity (Ex. 1.054)
+  */
+  public static function attenuation($og, $fg){
+    if($og > $fg && is_numeric($og) && is_numeric($fg))
+      return ($og - $fg)/($og - 1);
+    else
+      return null;
+  }
+
+  /* GRAVITY UNITS
+  #  param g: number - gravity
+  */
+  public static function gu($g){
+    if(is_numeric($g))
+      return round(($g - 1) * 1000);
+    else
+      return null;
+  }
+
+  /* TOTAL GRAVITY
+  # param g: number - gravity
+  # param vol: number - volume in gallons
+  */
+  public static function totalGravity($g, $v){
+    if(is_numeric($g) && is_numeric($v))
+      return self::gu($g) * $v;
+    else
+      return null;
+  }
+
+  /* FINAL GRAVITY
+  #  param g: number - initial gravity
+  #  param vol_beg: number - volume in gallons at the begining of the boil
+  #  param vol_end: number - volume in gallons at the end of the boil
+  */
+  public static function finalGravity($g, $vol_beg, $vol_end){
+    if(is_numeric($g) && is_numeric($vol_beg) && is_numeric($vol_end))
+      return self::totalGravity($g, $vol_beg) / $vol_end;
+    else
+      return null;
+  }
+
+  /* EXTRACT ADDITION
+  # param target_gu: number - Target Total Gravity in Gravity Units
+  # param total_gu: number - Total Gravity from Mash in Gravity Units
+  # param extract: string/number - should be 'LME' or 'DME' or custom value
+  */
+  public static function extractAddition($target_gu, $total_gu, $extractType){
+    if($extractType == 'LME')
+      $extract = 38;
+    elseif($extractType == 'DME')
+      $extract = 45;
+    elseif(is_numeric($extractType))
+      $extract = $extractType;
+
+    if(is_numeric($target_gu) && is_numeric($total_gu) && is_numeric($extract))
+      return ($target_gu - $total_gu) / $extract;
+    else
+      return null;
+  }
+
+  /* GRAVITY TEMPERATURE CORRECTION
+  # param temp: number - Temperature in Fahrenheit
+  # param g: number - Gravity from the hydrometer reading
+  */
+  public static function gravityCorrection($temp, $g){
+    if(is_numeric($temp) && is_numeric($g))
+      return ((1.313454 - 0.132674*$temp + 2.057793*pow(10,-3)*pow($temp,2) - 2.627634*pow(10,-6)*pow($temp,3)) * 0.001) + $g;
+    else
+      return null;
+  }
+}
---
+++ tests/unit/BeercalcTest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Beercalc;
+
+class BeercalcTest extends \PHPUnit_Framework_TestCase {
+
+  public function testABV(){
+    $this->assertEquals(7.204999999999992, Beercalc::abv(1.055, 1));
+    $this->assertEquals(null, Beercalc::abv(1, 1.055));
+    $this->assertEquals(null, Beercalc::abv("asdf", "asdf"));
+  }
+
+  public function testABW(){
+    $this->assertEquals(5.691949999999994, Beercalc::abw(1.055, 1));
+    $this->assertEquals(null, Beercalc::abw(1, 1.055));
+    $this->assertEquals(null, Beercalc::abw("asdf", "asdf"));
+  }
+
+  public function testMCU(){
+    $this->assertEquals(null, Beercalc::mcu(1, 1, 0));
+    $this->assertEquals(null, Beercalc::mcu(1, 1, null));
+    $this->assertEquals(null, Beercalc::mcu(null, 1, 1));
+    $this->assertEquals(null, Beercalc::mcu(1, null, 1));
+    $this->assertEquals(null, Beercalc::mcu(null, null, null));
+    $this->assertEquals(5, Beercalc::mcu(5, 5, 5));
+    $this->assertEquals(5.5, Beercalc::mcu(5.5, 5.5, 5.5));
+  }
+
+  public function testSRM(){
+    $this->assertEquals(null, Beercalc::srm(null, null, null));
+    $this->assertEquals(null, Beercalc::srm("asdf", null, "asdf"));
+    $this->assertEquals(5.668651803424155, Beercalc::srm(7, 5, 5));
+    $this->assertEquals(5.943353419684101, Beercalc::srm(7.5, 5.5, 5.5));
+  }
+
+  public function testAAU(){
+    $this->assertEquals(null, Beercalc::aau(null, 5));
+    $this->assertEquals(null, Beercalc::aau(4, null));
+    $this->assertEquals(null, Beercalc::aau(null, null));
+    $this->assertEquals(null, Beercalc::aau("asdf", "asdf"));
+    $this->assertEquals(9.600000000000001, Beercalc::aau(1.5, 6.4));  // Based off Palmer's Calculation
+    $this->assertEquals(4.6, Beercalc::aau(1, 4.6));  // Based off Palmer's Calculation
+  }
+
+  public function testUtilization(){
+    $this->assertEquals(0.08363227080582435, Beercalc::utilization(10, 1.050));
+    $this->assertEquals(0.30113013986478654, Beercalc::utilization(120, 1.030));
+    $this->assertEquals(0.0, Beercalc::utilization(0, 1.070));
+    $this->assertEquals(0.14780486892282785, Beercalc::utilization(45, 1.090));
+    $this->assertEquals(null, Beercalc::utilization(null, null));
+  }
+
+  public function testIBU(){
+    $this->assertEquals(25.365869680614512, Beercalc::ibu(6.4, 1.5, 60, 1.080, 5)); //Based off Palmer's Calculation
+    $this->assertEquals(6.03108750923272, Beercalc::ibu(4.6, 1, 15, 1.080, 5)); //Based off Palmer's Calculation
+    $this->assertEquals(null, Beercalc::ibu(null, 1, 15, 1.080, 5));
+    $this->assertEquals(null, Beercalc::ibu(null, null, 15, 1.080, 5));
+    $this->assertEquals(null, Beercalc::ibu(null, null, null, 1.080, 5));
+    $this->assertEquals(null, Beercalc::ibu(null, null, null, null, 5));
+    $this->assertEquals(null, Beercalc::ibu(null, null, null, 1.080, null));
+    $this->assertEquals(null, Beercalc::ibu(4.6, 1, 15, 1.080, "asdf"));
+  }
+
+  public function testPlato(){
+    $this->assertEquals(17.055185000000108, Beercalc::plato(1.070));  // Based on http://hbd.org/ensmingr/
+    $this->assertEquals(null, Beercalc::plato(null));
+    $this->assertEquals(null, Beercalc::plato("asdf"));
+  }
+
+  public function testRealExtract(){
+    $this->assertEquals(6.216277095999994, Beercalc::realExtract(1.070, 1.015));  // Based on http://hbd.org/ensmingr/
+    $this->assertEquals(null, Beercalc::realExtract(1.015, 1.070));
+    $this->assertEquals(null, Beercalc::realExtract(null, null));
+    $this->assertEquals(null, Beercalc::realExtract("asdf", "asdf"));
+  }
+
+  public function testCalories(){
+    $this->assertEquals(227.57821703464833, Beercalc::calories(1.070, 1.015));  // Based on http://hbd.org/ensmingr/
+    $this->assertEquals(null, Beercalc::calories(1.015, 1.070));
+    $this->assertEquals(null, Beercalc::calories(null, null));
+    $this->assertEquals(null, Beercalc::calories("asdf", "asdf"));
+  }
+
+  public function testAttenuation(){
+    $this->assertEquals(0.7777777777777778, Beercalc::attenuation(1.054, 1.012));
+    $this->assertEquals(null, Beercalc::attenuation(1, 1.055));
+    $this->assertEquals(null, Beercalc::attenuation("asdf", "asdf"));
+    $this->assertEquals(null, Beercalc::attenuation(null, null));
+  }
+
+  public function testGravityUnits(){
+    $this->assertEquals(54, Beercalc::gu(1.054));
+    $this->assertEquals(null, Beercalc::gu("asdf"));
+    $this->assertEquals(null, Beercalc::gu(null));
+  }
+
+  public function testTotalGravity(){
+    $this->assertEquals(270, Beercalc::totalGravity(1.054, 5));
+    $this->assertEquals(null, Beercalc::totalGravity("asdf", "asdf"));
+    $this->assertEquals(null, Beercalc::totalGravity(null, null));
+  }
+
+  public function testFinalGravity(){
+    $this->assertEquals(54, Beercalc::finalGravity(1.054, 5, 5));
+    $this->assertEquals(null, Beercalc::finalGravity("asdf", "asdf", "asdf"));
+    $this->assertEquals(null, Beercalc::finalGravity(null, null, null));
+  }
+
+  public function testExtractAddition(){
+    $this->assertEquals(1.1777777777777778, Beercalc::extractAddition(408, 355, 45));
+    $this->assertEquals(1.1777777777777778, Beercalc::extractAddition(408, 355, 'DME'));
+    $this->assertEquals(1.394736842105263, Beercalc::extractAddition(408, 355, 'LME'));
+    $this->assertEquals(null, Beercalc::extractAddition("asdf", "asdf", 'LME'));
+    $this->assertEquals(null, Beercalc::extractAddition("asdf", "asdf", 'asdf'));
+  }
+
+  public function testGravityCorrection(){
+    $this->assertEquals(1.0560765751842796, Beercalc::gravityCorrection(100.4, 1.050));
+    $this->assertEquals(null, Beercalc::gravityCorrection(100.4, "asdf"));
+    $this->assertEquals(null, Beercalc::gravityCorrection("asdf", 1.050));
+    $this->assertEquals(null, Beercalc::gravityCorrection("asdf", "asdf"));
+  }
+}
